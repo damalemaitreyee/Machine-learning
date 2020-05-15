eef63fe60b9c91106cb0804b2cd348c0949f6197
damalemaitreyee <30566781+damalemaitreyee@users.noreply.github.com>
2020-05-15 17:47:43 +0530

    Add files via upload

---
+++ plotting.py
@@ -0,0 +1,11 @@
+from capture import df
+from bokeh.plotting import figure, show, output_file
+
+p= figure(x_axis_type='datetime', height=100, width=500, sizing_mode="scale_width", title="Motion graph")
+p.yaxis.minor_tick_line_color=None
+p.ygrid[0].ticker.desired_num_ticks=1
+
+q=p.quad(left=df["Start"],right=df["End"],bottom=0,top=1,color="green")
+
+output_file("Graph.html")
+show(p)
